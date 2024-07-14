9e26f78d426f1e203b9338369f930014032d8290
Byron Gregorio Calderón López <e1350967533@live.uleam.edu.ec>
2024-07-14 16:28:16 -0500

    Create tipoDato.py
    awda

---
+++ codigos/tipoDato.py
@@ -0,0 +1,33 @@
+# Ejemplos de tipos de datos con el método type()
+
+# Entero (int)
+numero_entero = 5
+print(type(numero_entero))  # Output: <class 'int'>
+
+# Flotante (float)
+numero_flotante = 3.14
+print(type(numero_flotante))  # Output: <class 'float'>
+
+# Booleano (bool)
+es_verdadero = True
+print(type(es_verdadero))  # Output: <class 'bool'>
+
+# Cadena de caracteres (str)
+nombre = 'Juan'
+print(type(nombre))  # Output: <class 'str'>
+
+# Lista (list)
+lista_numeros = [1, 2, 3]
+print(type(lista_numeros))  # Output: <class 'list'>
+
+# Tupla (tuple)
+mi_tupla = (1, 'a', 3.14)
+print(type(mi_tupla))  # Output: <class 'tuple'>
+
+# Diccionario (dict)
+mi_diccionario = {'nombre': 'Juan', 'edad': 30}
+print(type(mi_diccionario))  # Output: <class 'dict'>
+
+# Conjunto (set)
+mi_conjunto = {1, 2, 3}
+print(type(mi_conjunto))  # Output: <class 'set'>
