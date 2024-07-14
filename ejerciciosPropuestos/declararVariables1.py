a424f06abf5ca076494653b4071b2a1022db70a9
Byron Gregorio Calderón López <e1350967533@live.uleam.edu.ec>
2024-07-14 13:09:44 -0500

    Create declararVariables1.py
    awdawd

---
+++ ejerciciosPropuestos/declararVariables1.py
@@ -0,0 +1,12 @@
+# declarar variables
+# nombre de tipo str
+nombre = 'Byron Calderón'
+# número de telefono
+celular = 989919578
+#correo
+correo ='coso@gmail.com'
+
+# imprimir valores por consola
+print('mi celular es:',celular)
+print('mi nombre es:',nombre)
+print('mi correo es:',correo)
