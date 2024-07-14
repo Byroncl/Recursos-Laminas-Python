90ce3884cad45ea5b69a286039672e04c84ebaf0
Byron Gregorio Calderón López <e1350967533@live.uleam.edu.ec>
2024-07-14 17:00:00 -0500

    Update cadenaTextoSuma.py
    adwa

--- codigos/cadenaTextoSuma.py
+++ codigos/cadenaTextoSuma.py
@@ -1,8 +1,10 @@
 numero1 = "1"
 numero2 = "2"
+print('suma #1')
 print("Concatenación:", numero1 + numero2)
 print("Suma:", int(numero1) + int(numero2))
-
+print("------------------------------")
+print('suma #2')
 numero3 = 1
 numero4 = 2
 print(numero1 + numero2)
