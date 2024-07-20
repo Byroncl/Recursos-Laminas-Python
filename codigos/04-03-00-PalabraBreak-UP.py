9cbabda635c1739c27f993a60177543af18f0507
Byron Gregorio Calderón López <e1350967533@live.uleam.edu.ec>
2024-07-20 13:26:10 -0500

    dwa

--- codigos/04-03-00-PalabraBreak-UP.py
+++ codigos/04-03-00-PalabraBreak-UP.py
@@ -1,6 +1,17 @@
+# codigo sin break
 for letra in 'Holanda':
+    if letra == 'a':
+        print(f'Letra encontrada: {letra}')
+
+else:
+    print('Fin ciclo for sin break')
+    
+print(f"--------------------------------------------------")
+# codigo con break
+for letra in 'Ecuador':
     if letra == 'a':
         print(f'Letra encontrada: {letra}')
         break
 else:
-    print('Fin ciclo for')
+    print('Fin ciclo for con break')
+    
